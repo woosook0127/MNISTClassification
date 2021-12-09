1a1aa3175b1025cc3196bfb56142b6b6b56a607b
comeeasy <zz1236zz@naver.com>
2021-12-09 15:26:11 +0900

    2021-12-09 python file and log upload

--- Code/Classifier_using_gap.py
+++ Code/Classifier_using_gap.py
@@ -12,17 +12,12 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 import os
-from PIL import Image
-import matplotlib.pyplot as plt
-import cv2
-import random
-
 
 # # Set Hyperparameter
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 torch.manual_seed(777)
-if device == "cuda":
+if device == "cuda:1":
     torch.cuda.manual_seed_all(777)
 print(f"using {device}")
 
@@ -82,7 +77,7 @@ def get_alphabet(root: str, batch_size: int):
                                  ]),
                                  target_transform=None)
     
-    train_loader = DataLoader(train1_rotation,
+    train_loader = DataLoader(train3_crop,
                               batch_size=batch_size,
                               shuffle=True,
                               drop_last=True,
@@ -97,19 +92,13 @@ def get_alphabet(root: str, batch_size: int):
     return (train1_rotation, train_loader, test_loader)
 
 
-# In[4]:
+data_path = "/home/r320/wooseok/MNISTClassification/Dataset/processed_data/"
 
-
-data_path = '/home/r320ws/Desktop/wooseok/MNISTClassification/Dataset/processed_data/'
 original_data, train_loader, test_loader = get_alphabet(data_path, batch_size)
 
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
-# In[5]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-def tensorToNumpy(tensor):
+'''def tensorToNumpy(tensor):
     c, w, h = tensor.shape
     
     img = np.zeros_like(tensor)
@@ -122,116 +111,9 @@ def tensorToNumpy(tensor):
 for i in random.choices(range(len(original_data)), k=10):
     image, target = original_data.__getitem__(i)
     tensorToNumpy(image)
-
+'''
 
 # # CNN Class
-
-# In[6]:
-
-
-#### 
-'''class CNN(nn.Module):  # train with rotated and adamw
-    def __init__(self):
-        a=32
-        b=128
-        c=512
-        d=1024
-        
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(         # 28x28
-            nn.Conv2d(1, a, 3, padding=1),
-            nn.BatchNorm2d(a),
-            nn.ReLU(inplace=True),
-        )
-        self.layer2 = nn.Sequential(          # 28x28      
-            nn.Conv2d(a, b, 3, padding=1),
-            nn.BatchNorm2d(b),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2,2)
-        )
-        self.layer3 = nn.Sequential(         #14x14
-            nn.Conv2d(b, c, 3, padding=1),
-            nn.BatchNorm2d(c),
-            nn.ReLU(inplace=True),  
-            nn.MaxPool2d(kernel_size=2, stride=2),
-                         
-            nn.Conv2d(c, d, 3, padding=1),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),  
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1)            
-        )
-        
-        self.fc_layer = nn.Sequential(
-            nn.Linear(in_features=d*4*4, out_features=1024),
-            nn.BatchNorm1d(1024),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            
-            nn.Linear(1024, 26)
-        )
-        
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc_layer(out)
-        return out'''
-
-'''
-## rotate, dropout04, optim_adam, kernel 5, out_feature = 512 ---> 99.56
-## rotate, dropout04, optim_adam, kernel 3, out_feature = 512 ---> 99.48
-class CNN(nn.Module):
-    def __init__(self):
-        a=32
-        b=128
-        c=512
-        
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(         # 28x28
-            nn.Conv2d(1, 128, 5, padding=2),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True)
-        )
-        self.layer2 = nn.Sequential(          # 28x28      
-
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.4)
-        )
-        self.layer3 = nn.Sequential(         #14x14
-
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.BatchNorm2d(c),
-            nn.MaxPool2d(2, 2, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.4)
-        )
-        
-        self.fc_layer = nn.Sequential(
-            nn.Linear(in_features=512*4*4, out_features=512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(0.4),
-            
-            nn.Linear(512, 26)
-        )
-        
-#         self.residual_layer = nn.Sequential(
-#             nn.Conv2d(1, 128, 1)
-#         )
-        
-    def forward(self, x):
-        out = self.layer1(x) 
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc_layer(out)
-        return out'''
-    
     
 ## rotate, dropout04, optim_adam, kernel all 5, out_feature = 512 ---> 99.56
 ## rotate, dropout04, optim_adam, kernel 3, out_feature = 512 ---> 99.48
@@ -250,7 +132,7 @@ class CNN(nn.Module):
         )
         self.layer2 = nn.Sequential(          # 28x28      
 
-            nn.Conv2d(128, 256, 5, padding=2),
+            nn.Conv2d(128, 256, 3, padding=2),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(2,2),
             nn.ReLU(inplace=True),
@@ -258,14 +140,14 @@ class CNN(nn.Module):
         )
         self.layer3 = nn.Sequential(         #14x14
 
-            nn.Conv2d(256, 512, 5, padding=2),
+            nn.Conv2d(256, 512, 3, padding=2),
             nn.BatchNorm2d(c),
             nn.MaxPool2d(2, 2, padding=1),
             nn.ReLU(inplace=True),
             nn.Dropout(0.4)
         )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(512, 26, kernel_size=5, padding=2),
+            nn.Conv2d(512, 26, kernel_size=3, padding=2),
             nn.BatchNorm2d(26),
             nn.AdaptiveAvgPool2d(1),
             nn.ReLU(),
@@ -301,135 +183,20 @@ class CNN(nn.Module):
 ## 
 
 
-
-'''class CNN(nn.Module):
-    def __init__(self):
-        a=32
-        b=128
-        c=512
-        
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(         # 28x28
-            nn.Conv2d(1, 128, 5, padding=2),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True)
-        )
-        self.layer2 = nn.Sequential(          # 28x28      
-
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.4)
-        )
-        self.layer3 = nn.Sequential(         #14x14
-
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.MaxPool2d(2, 2, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.4)
-        )
-        self.fc_layer = nn.Sequential(
-            nn.Linear(in_features=512*4*4, out_features=512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(0.4),
-            
-            nn.Linear(512, 26)
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(512, 26, kernel_size=5, padding=2),
-            nn.BatchNorm2d(26),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d(1),
-            nn.Dropout(0.4)
-        )
-        
-        
-#         self.residual_layer = nn.Sequential(
-#             nn.Conv2d(1, 128, 1)
-#         )
-        
-    def forward(self, x):
-        out = self.layer1(x) 
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = out.view(out.size(0), -1)
-        return out
-'''
-    
-'''   0.991
-class CNN(nn.Module):
-    def __init__(self):
-        a=32
-        b=64
-        c=512
-        
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(         # 28x28
-            nn.Conv2d(1, a, 3, padding=1),
-            nn.BatchNorm2d(a),
-            nn.ReLU(inplace=True),
-        )
-        self.layer2 = nn.Sequential(          # 28x28      
-            nn.Conv2d(a, a, 3, padding=1),
-            nn.BatchNorm2d(a),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2,2)
-        )
-        self.layer3 = nn.Sequential(         #14x14
-            nn.Conv2d(a, b, 3, padding=1),
-            nn.BatchNorm2d(b),
-            nn.ReLU(inplace=True),  
-            
-            nn.Conv2d(b, b, kernel_size=3, padding=1),    #7x7
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.fc_layer = nn.Sequential(
-            nn.Linear(in_features=64*7*7, out_features=512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            
-            nn.Linear(512, 26)
-        )
-        
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc_layer(out)
-        return out'''
-
-
 # # Define model
-
-# In[7]:
-
 
 model = CNN().to(device)
 
 # train1_2_ pretrained
-# model = torch.load("./weights/rotate_drpout04_adamoptim_kernel5_126ep_acc99.5619888305664.pth").to(device)
-
 
 loss_func = nn.CrossEntropyLoss().to(device)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=10)
-# optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-1)
-# scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=1e-5)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+# scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=10)
+optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-1)
+scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=1e-5)
 
 
 # # Training
-
-# In[8]:
-
 
 def test_acc(model, test_loader=test_loader):
     correct = 0
@@ -466,9 +233,9 @@ for i in range(1, num_epoch+1):
     accuracy = float(test_acc(model))
     if (accuracy> max_accuracy and accuracy>99.5):
         max_accuracy = accuracy
-        model_path = f"../weights/processed_Rotate_GAP_adam_reduceLR_all5kernel{i:03d}ep_acc{max_accuracy}.pth"
+        model_path = f"/home/r320/wooseok/MNISTClassification/weights/Ourdata_GAP_adam_reduceLR_first5kernel{i:03d}ep_acc{max_accuracy}.pth"
         torch.save(model, model_path)
     
-    scheduler.step(accuracy)
+    # scheduler.step(accuracy)
     print(f"Epoch: {i}, Loss: {loss.item()}, LR: {scheduler.optimizer.state_dict()['param_groups'][0]['lr']}, Acc: {accuracy}, Best: {max_accuracy}")
 
